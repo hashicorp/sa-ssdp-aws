4a5d86ad11575416338a93a6141aa8b5be98f998
n8 <npearce@hashicorp.com>
2023-01-31 23:31:34 +0000

    Need these for vault auto_auth role binding

--- platform/consul-ent-aws/outputs.tf
+++ platform/consul-ent-aws/outputs.tf
@@ -3,3 +3,12 @@ output "asg_name" {
   description = "Name of the AWS Auto-Scale Group serving the Consul Cluster"
   value = module.vm.asg_name
 }
+
+
+output "aws_consul_iam_role_arn" {
+  value = module.iam.aws_consul_iam_role_arn
+}
+
+output "aws_iam_instance_profile" {
+  value = module.iam.aws_iam_instance_profile
+}
