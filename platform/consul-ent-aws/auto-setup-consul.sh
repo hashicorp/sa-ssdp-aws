f224da63f64e9ba82a405b7a3921b486f62ad33c
n8 <npearce@hashicorp.com>
2023-01-31 23:31:06 +0000

    Start auto-setup-consul.sh, 'cause some of us don't have time....

---
+++ platform/consul-ent-aws/auto-setup-consul.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ] || [ -z $VAULT_ADDR ] || [ -z $VAULT_CACERT ]
+then
+   echo -e "Required env vars:\n\tAWS_ACCESS_KEY_ID\n\tAWS_SECRET_ACCESS_KEY\n\tVAULT_ADDR\n\tVAULT_CACERT"
+else
+
+#  AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -state /root/terraform/iam/terraform.tfstate aws_consul_iam_role_arn)
+  AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -raw aws_consul_iam_role_arn)
+  vault write auth/aws/role/consul auth_type=iam \
+    bound_iam_principal_arn="${AWS_CONSUL_IAM_ROLE_ARN}" \
+    policies=consul,admin ttl=30m
+
+fi
