4b3ebf694064fe7c67d00a5d492f134930c40fa7
n8 <npearce@hashicorp.com>
2023-02-01 20:55:43 +0000

    write policy

--- platform/consul-ent-aws/auto-setup-consul.sh
+++ platform/consul-ent-aws/auto-setup-consul.sh
@@ -5,6 +5,8 @@ then
    echo -e "Required env vars:\n\tAWS_ACCESS_KEY_ID\n\tAWS_SECRET_ACCESS_KEY\n\tVAULT_ADDR\n\tVAULT_CACERT"
 else
 
+  vault policy write consul policies/consul.hcl
+
 #  AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -state /root/terraform/iam/terraform.tfstate aws_consul_iam_role_arn)
   AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -raw aws_consul_iam_role_arn)
   vault write auth/aws/role/consul auth_type=iam \
