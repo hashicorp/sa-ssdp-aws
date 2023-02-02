518004ae646362296ac5c20f4ce913f85c179d33
n8 <npearce@hashicorp.com>
2023-02-02 17:56:57 +0000

    add connect policy and var validation

--- platform/consul-ent-aws/auto-setup-consul.sh
+++ platform/consul-ent-aws/auto-setup-consul.sh
@@ -6,11 +6,16 @@ then
 else
 
   vault policy write consul policies/consul.hcl
+  vault policy write connect policies/connect.hcl
 
-#  AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -state /root/terraform/iam/terraform.tfstate aws_consul_iam_role_arn)
   AWS_CONSUL_IAM_ROLE_ARN=$(terraform output -raw aws_consul_iam_role_arn)
-  vault write auth/aws/role/consul auth_type=iam \
-    bound_iam_principal_arn="${AWS_CONSUL_IAM_ROLE_ARN}" \
-    policies=consul,admin ttl=30m
-
+  if [ -z $AWS_CONSUL_IAM_ROLE_ARN ]
+  then
+    echo -e "Unable to retrieve Consul IAM Role ARN:\n\techo\$AWS_CONSUL_IAM_ROLE_ARN: $AWS_CONSUL_IAM_ROLE_ARN"
+  else
+    vault write auth/aws/role/consul auth_type=iam \
+      bound_iam_principal_arn="${AWS_CONSUL_IAM_ROLE_ARN}" \
+      policies=consul,admin ttl=30m
+  fi
 fi
+
