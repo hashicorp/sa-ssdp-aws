5615f7f291f1a1970266aa24d23b5e4b96aea958
Nathan Pearce <nathan.pearce@gmail.com>
2023-02-02 20:27:53 -0800

    cleanup providers, and pin versions

--- platform/consul-ent-aws/providers.tf
+++ platform/consul-ent-aws/providers.tf
@@ -16,10 +16,3 @@ provider "aws" {
   # your AWS region
   region = var.region
 }
-
-# //FIXME: removed for local testing
-#provider "vault" {
-#  # Using VAULT_ADDR
-#  # Using VAULT_TOKEN
-#  # Using VAULT_CACERT
-#}
