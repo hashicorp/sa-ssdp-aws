9819d42edcc729f734b486864a2977a6bda0730c
Nathan Pearce <nathan.pearce@gmail.com>
2023-02-14 11:33:45 -0800

    private subnets

--- infrastructure/generate-tfvars.tf
+++ infrastructure/generate-tfvars.tf
@@ -1,7 +1,7 @@
 resource "local_sensitive_file" "generate_tfvars" {
   content = templatefile("${path.module}/templates/generate-tfvars.tpl", {
     region                   = var.region
-    instance_subnets         = jsonencode(module.vpc_platform_services.public_subnets.*)
+    instance_subnets         = jsonencode(module.vpc_platform_services.private_subnets.*)
     vpc_platform_services_id = module.vpc_platform_services.vpc_id
     key_name                 = module.key_pair.key_pair_key_name
     })
